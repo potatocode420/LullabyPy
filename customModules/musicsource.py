efdb420a2517dc570681bab23ad5312aff1bd124
Marc Chai <marcchai123@gmail.com>
2022-03-13 07:05:18 +0800

    Added debug. Added more help

--- customModules/musicsource.py
+++ customModules/musicsource.py
@@ -37,6 +37,7 @@ class MusicSource(discord.PCMVolumeTransformer):
     def from_url(self, url, stream=True, loop=False):
         url = self.search(url)
         data = self.ytdl.extract_info(url, download=False)
+        print(data)
         if data["entries"]:
             data = data['entries'][0]
             filename = data['url'] if stream else self.ytdl.prepare_filename(data)
